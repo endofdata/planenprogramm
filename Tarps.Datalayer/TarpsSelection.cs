8338c27388fdeab2f26215c49979b79f2616fc0d
endofdata <endofdata@hotmail.de>
2023-11-03 19:52:03 +0100

    draft of selection class as used in Python app

---
+++ Tarps.Datalayer/TarpsSelection.cs
@@ -0,0 +1,19 @@
+﻿namespace Tarps.Datalayer
+{
+    public class TarpsSelection
+    {
+        private readonly HashSet<int> _selectedNumbers = new();
+
+        public string SelectedNumbers => string.Join(", ", _selectedNumbers.Select(n => n.ToString()));
+
+        public IDictionary<string, string> Selectors
+        {
+            get;
+        } = new Dictionary<string, string>();
+
+        public IDictionary<string, bool> Sequence
+        {
+            get;
+        } = new Dictionary<string, bool>();
+    }
+}
